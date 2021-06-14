49a76a50bc3aa8d9e92cb70455ebb4919e5b1a6c
abhijeet <abhijeet.thakur1@gmail.com>
2021-06-14 23:07:37 +0530

    Schoon-set up master directory

---
+++ myprivateequity/src/main/java/com/vanguard/retail/myprivateequity/webservice/MyprivateequityApplication.java
@@ -0,0 +1,13 @@
+package com.vanguard.retail.myprivateequity.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprivateequityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprivateequityApplication.class, args);
+	}
+
+}
---
+++ myprivateequity/src/test/java/com/vanguard/retail/myprivateequity/webservice/MyprivateequityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.retail.myprivateequity.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprivateequityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
