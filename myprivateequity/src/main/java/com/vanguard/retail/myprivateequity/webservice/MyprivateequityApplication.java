92107675201c3ee35e02b2f4b214be77b4a76585
abhijeet <abhijeet.thakur1@gmail.com>
2021-06-14 23:19:07 +0530

    master changes

--- myprivateequity/src/main/java/com/vanguard/retail/myprivateequity/webservice/MyprivateequityApplication.java
+++ myprivateequity/src/main/java/com/vanguard/retail/myprivateequity/webservice/MyprivateequityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyprivateequityApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MyprivateequityApplication.class, args);
 	}
 
